6cbd74ed17b3889626d8750b8cfb06d7eb9c2465
soham0209 <soham.juetce@gmail.com>
2021-04-02 15:53:54 -0400

    Added tag

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = {highlight_style: "dracula", theme: "black"};
 
-  // ns-hugo:/var/folders/v0/rgwg4h316plcc4fd9htxnkhh0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210322004751-17505c4581cd/assets/js/wowchemy-utils.js
+  // ns-hugo:/var/folders/v0/rgwg4h316plcc4fd9htxnkhh0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210324194200-fda9f39d872e/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
